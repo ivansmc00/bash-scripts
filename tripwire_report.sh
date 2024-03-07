92b892bc40ceae9356a1ad127c351d977335c03f
ivansmc00 <ivansmc00@gmail.com>
2024-03-07 10:06:58 +0100

    Send a PDF with a Tripwire report via e-mail

--- tripwire_report.sh
+++ tripwire_report.sh
@@ -4,16 +4,18 @@
 #==================================================#
 #====== Script to send a Tripwire via e-mail ======#
 #==================================================#
-
+#
+# MANDATORY: It's necessary to install mutt package if mailutils version don't support -A flag
+#
 #----- Variables -----#
 REPORTDIR="report_tmp"
 REPORTTXT="report.txt"
 REPORTPS="report.ps"
 REPORTPDF="tripwire_report_$(date +%d)-$(date +%m)-$(date +%Y).pdf"
-EMAIL="foo-bar@example.com" #<--- Change This
+EMAIL="fail2banivan@gmail.com" #<--- Change This
 
 #----- Specifies that SSMTP will be the binary used for mail sending -----#
-export SENDMAIL=/usr/sbin/ssmtp #<--- Change if necessary
+#export SENDMAIL=/usr/sbin/ssmtp #<--- Uncomment this just in case of using a mailutils version with -A flag supported
 
 #----- Temporal directory creation on /tmp -----#
 /usr/bin/mkdir /tmp/$REPORTDIR && /usr/bin/chmod 700 /tmp/$REPORTDIR;
@@ -28,7 +30,10 @@ export SENDMAIL=/usr/sbin/ssmtp #<--- Change if necessary
 /usr/bin/ps2pdf /tmp/$REPORTDIR/$REPORTPS /tmp/$REPORTDIR/$REPORTPDF;
 
 #----- Sending PDF attached on the mail -----#
-/usr/bin/echo "Informe diaro de Tripwire - $(date "+%A, %d de %B de %Y, %H:%M:%S")" | /usr/bin/mail -s "[Tripwire] Informe diario - $(date +%d)/$(date +%m)/$(date +%Y)" -A /tmp/$REPORTDIR/$REPORTPDF $EMAIL
+/usr/bin/echo "Informe diario de Tripwire - $(date '+%A, %d de %B de %Y, %H:%M:%S')" | /usr/bin/mutt -s "[Tripwire] Informe diario - $(date +%d)/$(date +%m)/$(date +%Y)" -a /tmp/$REPORTDIR/$REPORTPDF -- $EMAIL
+#/usr/bin/echo "Informe diario de Tripwire - $(date '+%A, %d de %B de %Y, %H:%M:%S')" | /usr/bin/mail -s "[Tripwire] Informe diario - $(date +%d)/$(date +%m)/$(date +%Y)" -A /tmp/$REPORTDIR/$REPORTPDF $EMAIL #<--- Uncomment this just in case of using a mailutils version with -A flag supported
 
 #----- Temporal directory remove with all its contents as well -----#
 /usr/bin/rm -rf /tmp/$REPORTDIR
+
+
